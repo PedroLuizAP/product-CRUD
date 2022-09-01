5c6bcb41c07b51720d47fa5c205298629cc1182d
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-09-01 16:14:52 -0300

    create basic crud

--- Program.cs
+++ Program.cs
@@ -2,38 +2,84 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+/*
 app.MapGet("/", () => "Hello World!"); //Get Hello Word example
 
-app.MapGet("/user", () => new{ Name="Pedro Luiz", Age=22}); //Get Json return example
+app.MapGet("/user", () => new { Name = "Pedro Luiz", Age = 22 }); //Get Json return example
 
 app.MapGet("/userHeader", (HttpResponse response) => response.Headers.Add("Name", "Pedro Luiz")); //Get Response Example
 
-app.MapPost("/saveproduct", (Product product) => {  //post example
-    return product.Id;
-});
 
 //app.MapGet("/GetAllProduct", (Product product) => {  //return object example
-  //  return product;
+//  return product;
 //});
 
-app.MapGet("/getproduct", ([FromQuery]string dateStart, [FromQuery]string dateEnd) => {  //get from query example
+app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) =>
+{  //get from query example
     return $"{dateStart}-{dateEnd}";
 });
 
-app.MapGet("/getproduct/{code}", ([FromRoute]string code) => { //get by route example
-    return code;
-});
-
-app.MapGet("/getproductbyheader", (HttpRequest request) => { // get by request example
+app.MapGet("/getproductbyheader", (HttpRequest request) =>
+{ // get by request example
     return request.Headers["product-code"];
 });
+*/
+
+app.MapGet("/getproduct/{id}", ([FromRoute] string id) =>
+{ //get by route example
+    var product = ProductRepository.GetById(id);
+    return product;
+});
 
 
+app.MapPost("/saveproduct", (Product product) =>
+{  //post example
+    ProductRepository.Add(product);
+});
+
+app.MapPut("/updateproduct", (Product product) =>
+{
+    var productSave = ProductRepository.GetById(product.Id);
+
+    if (productSave != null)
+        productSave.Name = product.Name;
+});
+
+app.MapDelete("/deleteproduct/{id}", ([FromRoute] string id) =>
+{ //get by route example
+    var product = ProductRepository.GetById(id);
+
+    if (product != null)
+        ProductRepository.Delete(product);
+});
 
 app.Run();
 
-public class Product {
+public static class ProductRepository
+{
+    public static List<Product> Products { get; set; }
+
+    public static void Add(Product product)
+    {
+        if (Products == null)
+            Products = new();
+
+        Products.Add(product);
+    }
+
+    public static Product? GetById(string id)
+    {
+        return Products.FirstOrDefault(product => product.Id == id);
+    }
+
+    public static void Delete(Product product)
+    {
+        Products.Remove(product);
+    }
+}
+
+public class Product
+{
     public string Id { get; set; }
     public string Name { get; set; }
 }
