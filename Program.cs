c271612fcf4df8dda1df368e76fa3ed562ef9a1a
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-09-28 20:29:22 -0300

    refactior

--- Program.cs
+++ Program.cs
@@ -4,17 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/product/{id}", ([FromRoute] string id) =>
-{ 
+{
     var product = ProductRepository.GetById(id);
 
-    if(product == null) return Results.NotFound();
-
+    if (product == null) return Results.NotFound();
 
     return Results.Ok(product);
 });
 
 app.MapPost("/product", (Product product) =>
-{  
+{
     ProductRepository.Add(product);
 
     return Results.Created($"/product/{product.Id}", product.Id);
@@ -24,16 +23,22 @@ app.MapPut("/product", (Product product) =>
 {
     var productSave = ProductRepository.GetById(product.Id);
 
-    if (productSave != null)
-        productSave.Name = product.Name;
+    if (productSave == null) return Results.NotFound();
+
+    productSave.Name = product.Name;
+
+    return Results.Ok();
 });
 
 app.MapDelete("/product/{id}", ([FromRoute] string id) =>
 {
     var product = ProductRepository.GetById(id);
 
-    if (product != null)
-        ProductRepository.Delete(product);
+    if (product == null) return Results.NotFound();
+
+    ProductRepository.Delete(product);
+
+    return Results.Ok();
 });
 
 app.Run();
@@ -52,7 +57,7 @@ public static class ProductRepository
 
     public static Product? GetById(string id)
     {
-        return Products.FirstOrDefault(product => product.Id == id);
+        return Products?.FirstOrDefault(product => product.Id == id);
     }
 
     public static void Delete(Product product)
