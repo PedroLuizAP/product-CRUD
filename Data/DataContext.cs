820bbecf2f650277b8af2198c20844e21b80c239
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-10-13 12:46:51 -0300

    Create DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using api.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("");
+        public DbSet<Product> Products { get; set; }
+    }
+}
